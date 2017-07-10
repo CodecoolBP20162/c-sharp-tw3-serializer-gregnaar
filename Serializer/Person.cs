14edb727cb542cc4d55310c28006532fbe203b13
AdamKovacs <kovacs.adam.mate@gmail.com>
2017-07-10 17:55:30 +0200

    First few tasks implemented

---
+++ Serializer/Person.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace Serializer
+{
+    [Serializable()]
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        private DateTime CreationDate { get; set; }
+        [NonSerialized]
+        private int SerialNumber;
+
+        public Person() { }
+
+        public Person(string name, string address, string phone)
+        {
+            Name = name;
+            Address = address;
+            Phone = phone;
+
+            CreationDate = DateTime.Now;
+            SerialNumber = 1; //need proper ID
+        }
+
+        public Person(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Address = (string)info.GetValue("Address", typeof(string));
+            Phone = (string)info.GetValue("Phone", typeof(string));
+            CreationDate = (DateTime)info.GetValue("CreationDate", typeof(DateTime));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Address", Address);
+            info.AddValue("Phone", Phone);
+            info.AddValue("CreationDate", CreationDate);
+        }
+
+        public void Serialize(string FilePath="Person.dat")
+        {
+            Stream stream = File.Open(FilePath, FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(stream, this);
+            stream.Close();
+        }
+
+        public static Person Deserialize(string FilePath = "Person.dat")
+        {
+            Stream stream = File.Open(FilePath, FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            Person person = (Person)bf.Deserialize(stream);
+            stream.Close();
+            return person;
+        }
+
+    }
+}
---
+++ Serializer/Serializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serializer
+{
+    public partial class SerializerForm : Form
+    {
+        public SerializerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SerializerForm_Load(object sender, EventArgs e)
+        {
+            Person person = Person.Deserialize();
+            RefreshPerson(person);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Person person = new Person(txtName.Text, txtAddress.Text, txtPhone.Text);
+            person.Serialize();
+            MessageBox.Show("Person saved!");
+        }
+
+        public void RefreshPerson(Person person)
+        {
+            txtName.Text = person.Name;
+            txtAddress.Text = person.Address;
+            txtPhone.Text = person.Phone;
+        }
+    }
+}
